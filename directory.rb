b11423426d31e4b324d99a939ad584e3dd7f6041
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-02 18:53:19 +0100

    Split interactive_method into additional methods, refactoring

--- directory.rb
+++ directory.rb
@@ -1,7 +1,8 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
 
   name = gets.chomp
 
@@ -12,61 +13,67 @@ def input_students
         cohort = "Cohort unknown"
       end
 
-    students << {name: name, cohort: cohort}
-      if students.count > 1
-        puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort}
+      if @students.count > 1
+        puts "Now we have #{@students.count} students"
       else
-        puts "Now we have #{students.count} student"
+        puts "Now we have #{@students.count} student"
       end
 
-    puts "Please enter the names of the students"
+    puts "Please enter the name of the student"
     name = gets.chomp
-  end
-  students
-end
-
-def print_header
-  puts "The students of Demo Academy"
-  puts "--------------"
-end
-
-def print(students)
-  students.each_with_index() do |student, i|
-    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50, " ")
-  end
-end
-
-def print_footer(students)
-  if students.count > 1
-    puts "Overall, we have #{students.count} great students".center(50, "-")
-  else
-    puts "Overall, we have #{students.count} great student".center(50, "-")
   end
 end
 
 def interactive_menu
-  students = []
-
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "Please enter a valid input"
-      end
-
-    end
+    print_menu
+    process(gets.chomp)
   end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Please enter a valid selection"
+  end
+end
+
+def print_header
+  puts "The Students of Demo Academy".center(50)
+  puts "-".center(50, "-")
+end
+
+def print_student_list
+  @students.each_with_index() do |student, i|
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50, " ")
+  end
+end
+
+def print_footer
+  if @students.count > 1
+    puts "Overall, we have #{@students.count} great students".center(50, "-")
+  else
+    puts "Overall, we have #{@students.count} great student".center(50, "-")
+  end
+end
 
 interactive_menu
