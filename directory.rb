9be28d6967702cc19b3a4f1d622f652443a1be20
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-02 19:37:21 +0100

    Add .gitignore & allowed saving student data to csv

--- directory.rb
+++ directory.rb
@@ -35,6 +35,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -50,6 +51,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -74,6 +77,16 @@ def print_footer
   else
     puts "Overall, we have #{@students.count} great student".center(50, "-")
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
