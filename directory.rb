a6532475da498a18cf84e81c917b57406f6529f7
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-08-02 18:23:17 +0100

    Added methods for entering and viewing data

--- directory.rb
+++ directory.rb
@@ -1,34 +1,47 @@
+# students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
   students = []
+
   name = gets.chomp
 
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+  while !name.empty?
+    puts "Now please enter the student's cohort"
+    cohort = gets.chomp
+      if cohort.empty?
+        cohort = "Cohort unknown"
+      end
+
+    students << {name: name, cohort: cohort}
+      if students.count > 1
+        puts "Now we have #{students.count} students"
+      else
+        puts "Now we have #{students.count} student"
+      end
+
+    puts "Please enter the names of the students"
     name = gets.chomp
   end
   students
 end
 
 def print_header
-  puts "The students of Villians Academy"
+  puts "The students of Demo Academy"
   puts "--------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index() do |student, i|
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50, " ")
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count > 1
+    puts "Overall, we have #{students.count} great students".center(50, "-")
+  else
+    puts "Overall, we have #{students.count} great student".center(50, "-")
+  end
 end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
